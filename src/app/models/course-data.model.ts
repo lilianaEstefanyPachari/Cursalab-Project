e8be173132e7217a2ed775e2375283c715acfef3
Lili <lilianapachari21@gmail.com>
2022-09-10 14:07:10 -0500

    feat(models): add interface for courses and instructors data

---
+++ src/app/models/course-data.model.ts
@@ -0,0 +1,13 @@
+export interface VideoData {
+  title: string;
+  instructor: string;
+}
+
+export interface ChapterData {
+  id?: string;
+  title: string;
+  url: string;
+  notes: string;
+  quiz: string;
+  resources: string;
+}
