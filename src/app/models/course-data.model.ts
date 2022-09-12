47c556671e9ffffffd675f528c62560bff9c87d4
Lili <lilianapachari21@gmail.com>
2022-09-11 23:04:10 -0500

    feat(video-player.component): add functionality to component

--- src/app/models/course-data.model.ts
+++ src/app/models/course-data.model.ts
@@ -1,10 +1,11 @@
-export interface VideoData {
+export interface CourseData {
   title: string;
   instructor: string;
 }
 
 export interface ChapterData {
   id?: string;
+  chapter: number;
   title: string;
   url: string;
   notes: string;
--- src/app/services/course-data.service.ts
+++ src/app/services/course-data.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import {
   Firestore,
   collectionData,
   collection,
   getDoc,
   doc,
+  orderBy,
 } from '@angular/fire/firestore';
 
 import { InstructorData } from './../models/instructor-data.model';
-import { VideoData, ChapterData } from '../models/course-data.model';
+import { CourseData, ChapterData } from '../models/course-data.model';
 
 @Injectable({
   providedIn: 'root',
@@ -17,14 +18,14 @@ import { VideoData, ChapterData } from '../models/course-data.model';
 export class CourseDataService {
   constructor(private db: Firestore) {}
 
-  async getCourseData(id: string): Promise<VideoData> {
+  async getCourseData(id: string): Promise<CourseData> {
     const docRef = doc(this.db, 'courses', id);
     const docSnap = await getDoc(docRef);
     const CourseExist = docSnap.exists();
     if (!CourseExist) {
       throw new Error("The Course doesn't exists");
     }
-    const courseData = docSnap.data() as VideoData;
+    const courseData = docSnap.data() as CourseData;
     return courseData;
   }
 
@@ -39,10 +40,38 @@ export class CourseDataService {
     return instructorData;
   }
 
-  getChaptersData(courseId: string): Observable<ChapterData[]> {
+  getAllChaptersData(courseId: string): Observable<ChapterData[]> {
     const collectionRef = collection(this.db, `courses/${courseId}/chapters`);
     return collectionData(collectionRef, { idField: 'id' }) as Observable<
       ChapterData[]
     >;
   }
+
+  async getChapterData(courseId: string, idDoc: string): Promise<ChapterData> {
+    const docRef = doc(this.db, `courses/${courseId}/chapters`, idDoc);
+    const docSnap = await getDoc(docRef);
+    const ChapterExist = docSnap.exists();
+    if (!ChapterExist) {
+      throw new Error("The Chapter doesn't exists");
+    }
+    const chapterData = docSnap.data() as ChapterData;
+    return chapterData;
+  }
+
+  // getCurrentChapter(
+  //   courseId: string,
+  //   chapterNumber: number
+  // ) {
+  //   return this.getAllChaptersData(courseId).subscribe({
+  //     next:(res) => {
+  //       console.log('todos los cursos: ', res);
+  //       const currentChapter= res.filter((e) => e.chapter === chapterNumber);
+  //       console.log(currentChapter);
+  //       return currentChapter;
+  //     },
+  //     error:(msg) => {
+  //       console.log('Error: ', msg);
+  //     },
+  //   });
+  // }
 }
